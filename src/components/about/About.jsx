1196f314bd9a521d20cd50690c971f3cff2e01aa
Lorenzzschmid <lorenz-schmid@gmx.net>
2022-07-08 17:40:42 +0200

    nav-icons added, vh for sections added

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import './about.css'
 const About = () => {
   return (
-    <div>About</div>
+    <section id='about'>
+      About
+    </section>
   )
 }
 
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,21 @@
 import React from 'react'; 
-import './nav.css'
+import './nav.css';
+import {AiOutlineHome} from 'react-icons/ai';
+import {AiOutlineUser} from 'react-icons/ai';
+import {BiBook} from 'react-icons/bi'; 
+import {RiServiceLine} from 'react-icons/ri'; 
+import {BiMessageSquareDetail} from 'react-icons/bi';
+
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#"><AiOutlineHome/></a>
+      <a href="#about"><AiOutlineUser/></a>
+      <a href="#experience"><BiBook/></a>
+      <a href="#services"><RiServiceLine/></a>
+      <a href="#contact"><BiMessageSquareDetail/></a>
+    </nav>
   )
 }
 
