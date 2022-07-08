fe6994e04ad80ce4304824dbc4128202fdd41096
Lorenzzschmid <lorenz-schmid@gmx.net>
2022-07-08 12:52:24 +0200

    foto added, social added in Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,25 @@
 import React from 'react'; 
+import CTA from './Cv';
 import './header.css'
+import ME from '../../assets/me.png'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="main-container header__container">
+        <h5>Hi I'm</h5>
+        <h1>Lorenz</h1>
+        <h5 className="text-lighter">Mernstack Developer</h5>
+        <CTA />
+
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+
+        <a href='#contact' className='scroll_down'>Scroll Down</a>
+
+      </div>
+    </header>
   )
 }
 
