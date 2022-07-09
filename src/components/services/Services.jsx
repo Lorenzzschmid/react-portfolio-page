1dfd8aec210d6c0281e63edf363b2747bb0b69e8
Lorenzzschmid <lorenz-schmid@gmx.net>
2022-07-09 14:52:09 +0200

    services added, services.jsx build with the list-items, css-grid implemented styling added

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,10 +1,136 @@
-import React from 'react'
-import './services.css'
+import React from "react";
+import "./services.css";
+import { BiCheck } from "react-icons/bi";
 
 const Services = () => {
   return (
-    <section id='services'>Services</section>
-  )
-}
+    <section id="services">
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
 
-export default Services
+      <div className="container services__container">
+        <article className="service">
+          <div className="service__head">
+            <h3>UI/UX Design</h3>
+          </div>
+          {/* ========== FIRST CARD ========== */}
+          <ul className="service__list">
+            {/* Card One - first */}
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            {/* Card One - second */}
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            {/* Card One - third */}
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            {/* Card One - fourth */}
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            {/* Card One - fifth */}
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            {/* Card One - sixt */}
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+            </li>
+          </ul>
+        </article>
+        {/* ========== SECOND CARD ========== */}
+        <article className="service">
+          <div className="service__head">
+            <h3>Web Development</h3>
+          </div>
+          <ul className="service__list">
+            {/* Second Card - first */}
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            {/* Second Card - second */}
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            {/* Second Card - third */}
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            {/* Second Card - fourth */}
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            {/* Second Card - fifth */}
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            {/* Second Card - sixt */}
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            {/* Second Card - seventh */}
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+            </li>
+          </ul>
+        </article>
+             {/* ========== THIRD CARD ========== */}
+             <article className="service">
+          <div className="service__head">
+            <h3>Content Creation</h3>
+          </div>
+          <ul className="service__list">
+            {/* Third Card - first */}
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            {/* Third Card - second */}
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            {/* Third Card - third */}
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            {/* Third Card - fourth */}
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            {/* Third Card - fifth */}
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            {/* Third Card - sixt */}
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+            </li>
+          </ul>
+        </article>
+      </div>
+    </section>
+  );
+};
+
+export default Services;
