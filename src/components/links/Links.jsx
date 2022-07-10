ef26e2eec6709a6b50ebd6a714692d8e1376d7b0
Lorenzzschmid <lorenz-schmid@gmx.net>
2022-07-10 13:28:15 +0200

    links added and styled

--- src/components/links/Links.jsx
+++ src/components/links/Links.jsx
@@ -7,6 +7,38 @@ import Link4 from '../../assets/link4.png';
 import Link5 from '../../assets/link5.png'; 
 import Link6 from '../../assets/link6.png';  
 
+const links = [
+  {
+    name: Link1,
+    link: "https://fireship.io",
+    description: "Fireship.io is a gateway drug for developers who want to build awesome web & mobile apps."
+  },
+  {
+    name: Link2,
+    link: "https://courses.webdevsimplified.com",
+    description: "On point explanations for learning Web Development"
+  },
+  {
+    name: Link3,
+    link: "A full Course on Cyber-Security",
+    description: "A fun way to learn cyber security. Hands-on security training through real-world scenarios."
+  },
+  {
+    name: Link4,
+    link: "https://developer.mozilla.org",
+    description: "Resources for Developers, by Developers."
+  },
+  {
+    name: Link5,
+    link: "https://codewars.com",
+    description: "Achieve Mastery Through Challenge."
+  },
+  {
+    name: Link6,
+    link: "https://frontendmasters.com",
+    description: "Advance your skills with In-Depth, modern Front-End engineering courses."
+  }
+]
 
 const Links = () => {
   return (
@@ -15,40 +47,22 @@ const Links = () => {
       <h2>Sources</h2>
 
       <div className="container links__container">
-        <article className="link">
-          <div className="link__preview">
-            <img src={Link1} alt="Link One" />
-            <h5 className='link__name'>Fireship</h5>
-            <small className='lnk__recommend'>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis ullam architecto nemo quam aut a. Autem rem velit placeat repudiandae! Dignissimos, a quaerat corrupti magnam quae quo soluta nam qui!
-            </small>
-          </div>
-        </article>
-        <article className="link">
-          <div className="link-preview">
-            <img src={Link1} alt="Link One" />
-            <h5 className='link__name'>Fireship</h5>
-            <small className='lnk__recommend'>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis ullam architecto nemo quam aut a. Autem rem velit placeat repudiandae! Dignissimos, a quaerat corrupti magnam quae quo soluta nam qui!
-            </small>
-          </div>
-        </article><article className="link">
-          <div className="link-preview">
-            <img src={Link1} alt="Link One" />
-            <h5 className='link__name'>Fireship</h5>
-            <small className='lnk__recommend'>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis ullam architecto nemo quam aut a. Autem rem velit placeat repudiandae! Dignissimos, a quaerat corrupti magnam quae quo soluta nam qui!
-            </small>
-          </div>
-        </article><article className="link">
-          <div className="link-preview">
-            <img src={Link1} alt="Link One" />
-            <h5 className='link__name'>Fireship</h5>
-            <small className='lnk__recommend'>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis ullam architecto nemo quam aut a. Autem rem velit placeat repudiandae! Dignissimos, a quaerat corrupti magnam quae quo soluta nam qui!
-            </small>
-          </div>
-        </article>
+        {
+          links.map(({name, link, description}, index) => {
+            return(
+            <article key={index} className="link">
+              <div className="link__preview">
+                <img src={name} alt="Recommendation" />
+              </div>
+                <h5 className='link__name'>{link}</h5>
+                <small className='lnk__recommend'>
+                {description}
+                </small>
+              
+            </article>
+            )
+          })
+        }
       </div>
     </section>
   )
