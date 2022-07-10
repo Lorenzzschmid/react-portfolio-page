bdba2ff1230867dad8e27d82ac1e005751042bf7
Lorenzzschmid <lorenz-schmid@gmx.net>
2022-07-10 16:42:01 +0200

    footer jsx edited, ankers created and social i.e. profile reference added

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,12 +1,29 @@
-import React from 'react'; 
-import './contact.css'; 
-import {MdOutlineEmail} from 'react-icons/md';  
-import {BsSignal} from 'react-icons/bs';
-import {BsMessenger} from 'react-icons/bs'; 
+import React from "react";
+import "./contact.css";
+import { MdOutlineEmail } from "react-icons/md";
+import { BsSignal } from "react-icons/bs";
+import { BsMessenger } from "react-icons/bs";
+import { useRef } from "react";
+import emailjs from "emailjs-com";
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_29g1tdc",
+        "template_zxga5no",
+        form.current,
+        "WHQ8C8y1oGEgjvacS"
+      )
+    e.target.reset()
+  };
+
   return (
-    <section id='contact'>
+    <section id="contact">
       <h5>Get In Touch</h5>
       <h2>Contact Me</h2>
 
@@ -14,36 +31,56 @@ const Contact = () => {
         <div className="contact__options">
           {/* Contact option 1 */}
           <article className="contact__option">
-            <MdOutlineEmail className='contact__option-icon'/>
+            <MdOutlineEmail className="contact__option-icon" />
             <h4>Email</h4>
-            <h5>lo.schmid@proton.me</h5> 
-            <a href="mailto:lo.schmid@proton.me" target="blank">Send a message</a>
+            <h5>lo.schmid@proton.me</h5>
+            <a href="mailto:lo.schmid@proton.me" target="blank">
+              Send a message
+            </a>
           </article>
           {/* Contact option 2 */}
           <article className="contact__option">
-            <BsSignal className='contact__option-icon'/>
+            <BsSignal className="contact__option-icon" />
             <h4>Signal</h4>
-            <h5>01573-1234567</h5> 
-            <a href="https://api.signal.com/send?phone+491573123456" target="blank">Send a message</a>
+            <h5>01573-1234567</h5>
+            <a
+              href="https://api.signal.com/send?phone+491573123456"
+              target="blank"
+            >
+              Send a message
+            </a>
           </article>
           {/* Contact option 3 */}
           <article className="contact__option">
-            <BsMessenger className='contact__option-icon'/>
+            <BsMessenger className="contact__option-icon" />
             <h4>Messenger</h4>
-            <h5>Webdev-Hero</h5> 
-            <a href="https://m.me/lo.schmid" target="blank">Send a message</a>
+            <h5>Webdev-Hero</h5>
+            <a href="https://m.me/lo.schmid" target="blank">
+              Send a message
+            </a>
           </article>
-
         </div>
-        <form action="">
-          <input type="text" name='name' placeholder='Your Full Name' required />
-          <input type="email" name='email' placeholder='Your Email' required />
-          <textarea name="message" rows="7" placeholder='Your Message' required ></textarea>
-          <button type='submit' className='btn btn-main'>Send Message</button>
+        <form ref={form} onSubmit={sendEmail}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-main">
+            Send Message
+          </button>
         </form>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,34 @@
 import React from 'react'; 
-import './footer.css'; 
+import './footer.css';
+import {AiFillTwitterSquare} from 'react-icons/ai'; 
+import {SiCodewars} from 'react-icons/si'; 
+import {SiTryhackme} from 'react-icons/si'; 
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>EGATOR</a>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#Experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio"></a></li>
+        <li><a href="#sources"></a></li>
+        <li><a href="#contact"></a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://twitter.com"></a>
+        <a href="https://www.codewars.com/users/Mighty%20Lo"></a>
+        <a href="https://tryhackme.com/profile"></a>
+      </div>
+      
+      <div className="footer__copyright">
+        <small>&copy; Made with 💚 by Lorenz. All rights reserved.</small>
+      </div>
+    </footer>
   )
 }
 
